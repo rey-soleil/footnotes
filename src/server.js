2cd03e920f6370d331cdae6708a01c090105c163
rey-soleil <68834278+rey-soleil@users.noreply.github.com>
2022-10-27 20:19:02 -0700

    GET /footnotes API

--- src/server.js
+++ src/server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.get("/users/:id", (req, res) => {
+app.get("/footnotes/:id", (req, res) => {
     const { id } = req.params;
-    res.send('rey test');
-    console.log(`rey requesting id ${id}`);
+    res.send(`the frontend should read ${id}`);
+    console.log(`the backend should read ${id}`);
 });
 
 app.listen(4000, () => console.log("Simple server running on http://localhost:4000"))
