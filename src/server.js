c33e9f99e397d1e9865920d5ae2cd3c96cd8620e
rey-soleil <68834278+rey-soleil@users.noreply.github.com>
2022-10-27 21:22:46 -0700

    cleanup server print statements

--- src/server.js
+++ src/server.js
@@ -4,8 +4,8 @@ const app = express();
 
 app.get("/footnotes/:id", (req, res) => {
     const { id } = req.params;
-    res.send(`expecting to see this in chrome dev tools`);
-    console.log(`the backend should read ${id}`);
+    res.send(`at localhost it should say id=${id}`);
+    console.log(`in node.js it should print id=${id}`);
 });
 
 app.listen(4000, () => console.log("Simple server running on http://localhost:4000"))
